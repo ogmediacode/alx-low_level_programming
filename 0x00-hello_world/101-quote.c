d5d75c579b47bee3c94f82c232e40754a04726b1
ogmediacode <geogeoni99@gmail.com>
2022-12-10 12:35:12 -0800

    Hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * *Return: 1
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-29\m", 59);
+return (1);
+}
