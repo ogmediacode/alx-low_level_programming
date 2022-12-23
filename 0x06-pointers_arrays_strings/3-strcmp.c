f0dd20f74a485c47a29ca577233b554de1ca299b
ogmediacode <geogeoni99@gmail.com>
2022-12-22 21:21:25 -0800

    flo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,16 +4,17 @@
 * _strcmp - compare string values
 *
 * @s1: input value
+*
 * @s2: input value
 *
-* Return: s1[i] - s2[i]
+* Return: s1[i] - s2[i
 */
 
 int _strcmp(char *s1, char *s2)
 {
 int i;
 i = 0;
-d
+
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
