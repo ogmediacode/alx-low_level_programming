a5a510593eb29373745d5f4d4552cc165794f975
ogmediacode <geogeoni99@gmail.com>
2022-12-26 06:04:37 -0800

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 void print_alphabet(void);
-void primt_alphabet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
