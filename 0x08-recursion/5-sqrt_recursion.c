f8fe6fd7a06688d878880d7015abd32c9ad7fc40
ogmediacode <geogeoni99@gmail.com>
2023-01-06 07:09:10 -0800

    recurs

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,10 +24,10 @@ return (actual_sqrt_recursion(n, 0));
 * actual_sqrt_recursion - recurses to find the natural
 *
 * square root of a number
+*
 * @n: number to calculate the sqaure root of
+*
 * @i: iterator
-*
-*
 *
 * Return: the resulting square root
 *
