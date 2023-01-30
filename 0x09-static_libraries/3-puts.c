888d21d16528f6b49db226ecbeac1d3d57f253d0
ogmediacode <geogeoni99@gmail.com>
2023-01-30 03:00:57 -0800

    bethel

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,8 @@
 #include "main.h"
-void _puts(char *str)
 #include <stdio.h>
 
 /**
 * main - Entry point
-* _puts - attends to the data
-* _puts - always in the data
 * Return: Always 0 (Success)
 *
 */
